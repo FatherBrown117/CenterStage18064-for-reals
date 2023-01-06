629dab616bd03c34372d5c41159da333ee840e02
GageGgfvhjnmk <gage.gerwick@ums.legacypca.org>
2023-01-06 12:22:42 -0600

    Roadrunner Variables mostly tunned ehh yo v24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,7 +34,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0, 14.721254006410254);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(19.75,0, 9, 14.721254006410254);
 
         //line used to calculate f coefficient; taken out because value determined empirically:
         //getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 16; // in
+    public static double TRACK_WIDTH = 13; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
